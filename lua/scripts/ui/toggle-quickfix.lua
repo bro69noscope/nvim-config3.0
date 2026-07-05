7b2beb34beb0ec2c8c86e73bc0010def6ae0a81d
Loïc Villet <villet.loic@gmail.com>
2026-07-05 15:24:20 +0200

    cleanup qf toggle

--- lua/scripts/ui/toggle-quickfix.lua
+++ lua/scripts/ui/toggle-quickfix.lua
@@ -1,17 +1,14 @@
 local M = {}
-M.toggle_quickfix = function()
-  local qf_exists = false
-  for _, win in pairs(vim.fn.getwininfo()) do
+
+function M.toggle_quickfix()
+  for _, win in ipairs(vim.fn.getwininfo()) do
     if win.quickfix == 1 then
-      qf_exists = true
+      vim.cmd.cclose()
+      return
     end
   end
-  if qf_exists == true then
-    vim.cmd("cclose")
-  else
-    vim.cmd("copen")
-  end
+
+  vim.cmd.copen()
 end
 
--- Toggle quickfix
 return M
