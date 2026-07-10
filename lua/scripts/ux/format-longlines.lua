dfdcece8edd014935e77adb7524a4c38ac6ebef2
Loïc Villet <villet.loic@gmail.com>
2026-07-10 11:25:36 +0200

    wip

---
+++ lua/scripts/ux/format-longlines.lua
@@ -0,0 +1,17 @@
+local M = {}
+M.makeshift_format = function()
+  local tw = vim.bo.textwidth
+  local ft = vim.bo.ft
+  if tw == 0 then
+    vim.notify("textwidth not set for ft" .. ft, error)
+    return
+  end
+
+  for i = vim.fn.line("$"), 1, -1 do
+    if #vim.fn.getline(i) > tw then
+      vim.fn.cursor(i, 1)
+      vim.cmd("normal! gww")
+    end
+  end
+end
+return M
