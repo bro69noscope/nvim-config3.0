2e42b87ace293f7eac2b51633673f12ac9ce8c3e
Loïc Villet <villet.loic@gmail.com>
2026-07-10 11:19:38 +0200

    linediff horizontal if lines too long

--- lua/plugins/linediff.lua
+++ lua/plugins/linediff.lua
@@ -44,12 +44,40 @@ return {
     "Linediff",
   },
   config = function()
+    local diff_sanitize = require("scripts.ui.diff-sanitize")
+
+    -- Horizontal diff if the line is long
+    local function split_long_linediff()
+      local longest_line = 0
+      local max_len = 70
+
+      for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
+        longest_line = math.max(longest_line, #line)
+      end
+
+      if longest_line > max_len then
+        vim.cmd("wincmd K")
+      end
+    end
+
+    local linediff_tabs = {}
     vim.api.nvim_create_autocmd("User", {
       pattern = "LinediffBufferReady",
       callback = function()
-        local diff_sanitize = require("scripts.ui.diff-sanitize")
+        local tab = vim.api.nvim_get_current_tabpage()
+
+        linediff_tabs[tab] = true
+
+        split_long_linediff()
         diff_sanitize.disable_diff_features()
-        vim.api.nvim_buf_set_keymap(0, "n", "q", ":LinediffReset<CR>", { noremap = true })
+
+        vim.keymap.set("n", "q", function()
+          vim.cmd("LinediffReset")
+          vim.cmd("tabclose")
+        end, {
+          buffer = 0,
+          silent = true,
+        })
 
         vim.keymap.set("n", "<C-j>", function()
           if vim.wo.diff then
@@ -62,12 +90,17 @@ return {
             vim.cmd.normal({ "[c", bang = true })
           end
         end, { buffer = 0 })
+      end,
+    })
 
-        vim.api.nvim_create_autocmd("TabClosed", {
-          callback = function()
+    vim.api.nvim_create_autocmd("TabClosed", {
+      callback = function()
+        for tab in pairs(linediff_tabs) do
+          if not vim.api.nvim_tabpage_is_valid(tab) then
+            linediff_tabs[tab] = nil
             diff_sanitize.re_enable_diff_features()
-          end,
-        })
+          end
+        end
       end,
     })
   end,
