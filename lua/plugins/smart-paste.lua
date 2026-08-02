48c7331a5b32e38702b9a29918da2a6f727f71f4
Loïc Villet <villet.loic@gmail.com>
2026-08-02 20:42:27 +0200

    fix (lol)

--- lua/plugins/smart-paste.lua
+++ lua/plugins/smart-paste.lua
@@ -2,7 +2,7 @@ return {
   "nemanjamalesija/smart-paste.nvim",
   event = "VeryLazy",
   config = function()
-    vim.keymap.set("n", "<M-p>", function()
+    vim.keymap.set({ "n", "v" }, "<M-p>", function()
       require("smart-paste").paste({ register = "+", key = "p" })
     end, { desc = "Smart paste from system clipboard" })
     vim.keymap.set("i", "<M-p>", "<C-r>+", { desc = "Paste from system clipboard" })
