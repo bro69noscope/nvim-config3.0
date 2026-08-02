89b48406eabf72b449acd78ec477da21379f154c
Loïc Villet <villet.loic@gmail.com>
2026-08-02 15:43:34 +0200

    add imode clip paste with m-p

--- lua/plugins/smart-paste.lua
+++ lua/plugins/smart-paste.lua
@@ -5,5 +5,6 @@ return {
     vim.keymap.set("n", "<M-p>", function()
       require("smart-paste").paste({ register = "+", key = "p" })
     end, { desc = "Smart paste from system clipboard" })
+    vim.keymap.set("i", "<M-p>", "<C-r>+", { desc = "Paste from system clipboard" })
   end,
 }
