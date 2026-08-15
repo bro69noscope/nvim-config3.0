c19413f3ebeb80429f230c476be86aafb2f9c389
Loïc Villet <villet.loic@gmail.com>
2026-08-15 16:17:57 +0200

    add csharp custom queries

---
+++ after/queries/c_sharp/textobjects.scm
@@ -0,0 +1,20 @@
+; extends
+
+; Capture function names in function calls
+(invocation_expression
+  function: (identifier) @function_name)
+
+; Capture method names in method calls
+(invocation_expression
+  function: (member_access_expression
+    name: (identifier) @method_name))
+
+; Capture both function name or method name
+(invocation_expression
+  function: [
+    (identifier) @call_name
+    (member_access_expression name: (identifier) @call_name)
+  ])
+
+(member_access_expression
+  name: (identifier) @variable.member.inner) @variable.member.outer
