297ed711b2c9bed84cf8fff73d8a5e21905db7dc
Loïc Villet <villet.loic@gmail.com>
2026-09-02 16:10:54 +0200

    more ps1 ts queries

--- after/queries/powershell/textobjects.scm
+++ after/queries/powershell/textobjects.scm
@@ -16,3 +16,21 @@
 (member_access
   (member_name) @variable.member.inner) @variable.member.outer
 
+(for_statement) @loop.outer
+(foreach_statement) @loop.outer
+(while_statement) @loop.outer
+(do_statement) @loop.outer
+
+(if_statement) @conditional.outer
+(switch_statement) @conditional.outer
+
+(comment) @comment.inner
+
+(attribute) @attribute.inner
+
+(integer_literal) @number.inner
+(real_literal) @number.inner
+
+(invokation_expression
+  (member_access
+    (member_name) @call.method_name))
